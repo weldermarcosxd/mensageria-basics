38ade3bc5c7fab6c0a1aff7aa64cb5c0d1255a7d
welder.silva <welder.silva@softplan.com.br>
2022-04-29 13:04:51 -0300

    sec 1

---
+++ src/Consumer/Receiver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory{HostName = "localhost"};
+            using var conncetion = factory.CreateConnection();
+            using var channel = conncetion.CreateModel();
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.Span;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Received message {message}");
+            };
+            
+            channel.BasicConsume("BasicTest", true, consumer);
+            Console.WriteLine("Press [enter] to exit the consumer app...");
+        }
+    }
+}
---
+++ src/Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory{HostName = "localhost"};
+            using var conncetion = factory.CreateConnection();
+            using var channel = conncetion.CreateModel();
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+            var message = "getting started";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", "BasicTest", null, body);
+            Console.WriteLine($"Sent message {message}");
+            Console.WriteLine("Press [enter] to exit the sender app...");
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
