b1bc6a3b1f7c9b8d26723b9eb4d34eebd5787c58
welder.silva <welder.silva@softplan.com.br>
2022-05-05 13:05:36 -0300

    publish

---
+++ micro-rabbit/src/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+    }
+}
